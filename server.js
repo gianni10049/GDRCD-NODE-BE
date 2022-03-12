be12b64390c8c2d4799399dd91bb5f1f65cdd8f6
gianni10049 <giovanni.pane10049@gmail.com>
2022-03-12 11:19:30 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+let { graphql, buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The rootValue provides a resolver function for each API endpoint
+let rootValue = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql({
+    schema,
+    source: '{ hello }',
+    rootValue
+}).then((response) => {
+    console.log(response);
+});
