b2ccb10f3d09f28d974bccf81699b9b9add6e73b
gianni10049 <giovanni.pane10049@gmail.com>
2022-03-15 16:15:42 +0100

    GDRCD_NODE [SEQUELIZE] - V1.1 Sequelize Migrations

--- resolver/homepage.js
+++ resolver/homepage.js
@@ -2,9 +2,7 @@ let { RolledDices } = require('./../models');
 
 const schema1 = {
 	hello: async () => {
-		await RolledDices.create({
-			rolled: 'test,test,test',
-		});
+		return 'Hello World!';
 	},
 };
 
