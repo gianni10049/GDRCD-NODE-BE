1b9ce7600d4928c675258ddd3a88922a2da256a4
gianni10049 <giovanni.pane10049@gmail.com>
2022-03-15 16:15:05 +0100

    GDRCD_NODE [SEQUELIZE] - V1.1 Sequelize Migrations

---
+++ config/config.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+try {
+	dotenv.config();
+} catch (e) {
+	console.log('missing env file', e);
+}
+
+module.exports = {
+	'development': {
+		'username': process.env.MYSQL_USER,
+		'password': process.env.MYSQL_PASS,
+		'database': process.env.MYSQL_DB,
+		'host': process.env.MYSQL_HOST,
+		'dialect': 'mysql',
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+	},
+	'test': {
+		'username': 'root',
+		'password': null,
+		'database': 'database_test',
+		'host': '127.0.0.1',
+		'dialect': 'mysql',
+	},
+	'production': {
+		'username': 'root',
+		'password': null,
+		'database': 'database_production',
+		'host': '127.0.0.1',
+		'dialect': 'mysql',
+	},
+};
