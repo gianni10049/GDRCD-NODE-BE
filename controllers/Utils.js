9f792c3fcad73d8cf0125331502c6695dcd74581
gianni10049 <giovanni.pane10049@gmail.com>
2022-03-18 00:07:41 +0100

    GDRCD_NODE [SMTP] - SMTP Service

--- controllers/Utils.js
+++ controllers/Utils.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { Account, Character } = require('./../models');
+const nodemailer = require('nodemailer');
 
 const createToken = async (data) => {
 	return await jwt.sign(data, process.env.TOKEN_KEY, undefined, undefined);
@@ -45,5 +46,44 @@ const verifyToken = async (token) => {
 	}
 };
 
+const sendMail = async (data) => {
+	let { subject, to, text, html } = data;
+
+	let transporter = nodemailer.createTransport({
+		host: process.env.SMTP_HOST,
+		port: process.env.SMTP_PORT,
+		secure: false, // true for 465, false for other ports
+		auth: {
+			user: process.env.SMTP_USER, // generated ethereal user
+			pass: process.env.SMTP_PASS, // generated ethereal password
+		},
+	});
+
+	await transporter.sendMail({
+		from: process.env.SMTP_SENDER, // sender address
+		to: to.join(','), // list of receivers
+		subject: subject, // Subject line
+		text: text, // plain text body
+		html: html, // html body
+	});
+
+	console.log('Mail Sended successfully!');
+};
+
+function makeRandomString(length) {
+	var result = '';
+	var characters =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(
+			Math.floor(Math.random() * charactersLength)
+		);
+	}
+	return result;
+}
+
 exports.validateToken = verifyToken;
 exports.createToken = createToken;
+exports.sendMail = sendMail;
+exports.makeRandomString = makeRandomString;
