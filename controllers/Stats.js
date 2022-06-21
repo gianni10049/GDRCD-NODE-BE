562f05a24ba9d78cd41f1472200bfb4e75121d7a
gianni10049 <giovanni.pane10049@gmail.com>
2022-06-21 13:34:11 +0200

    GDRCD_NODE [STATS] - Update stats v2

--- controllers/Stats.js
+++ controllers/Stats.js
@@ -1,6 +1,7 @@
 let { Stats, CharacterPoints, CharacterStats } = require('./../models');
 const { i18n } = require('../i18n');
 const { Sequelize } = require('sequelize');
+const { PermissionController } = require('./Permission');
 
 class StatsController {
 	static async listStats() {
@@ -8,47 +9,82 @@ class StatsController {
 	}
 
 	static async updateStat(args) {
-		let { character, stat } = args;
+		let { tokenData, character, stat } = args;
 
 		let response = false,
 			responseStatus;
 
-		let points = await CharacterPoints.findOne({
-			where: {
-				character: character,
-			},
+
+		let permission = await PermissionController.isMineCharacter({
+			tokenData: tokenData,
+			characterId: character,
 		});
 
-		if (points) {
-			let stat_points = points?.stat_points;
+		if (!permission.response) {
+			permission = await PermissionController.permissionControl({
+				tokenData: tokenData,
+				permission: 'MANAGE_STAT_OTHER',
+			});
+		}
 
 
-			if (stat_points > 0) {
+		if(permission) {
+			let points = await CharacterPoints.findOne({
+				where: {
+					character: character,
+				},
+			});
 
-				let char_stat = await CharacterStats.findOne({
-					where: {
-						character,
-						stat,
-					},
-				});
+			if (points) {
+				let stat_points = points?.stat_points;
 
-				if (char_stat) {
-					let next_level = Number(char_stat.value + 1);
 
-					if( next_level > 0 && next_level < 6 ) {
-						await CharacterStats.update({
-							value: Number(char_stat.value + 1),
-						}, {
-							where: {
-								character,
-								stat,
-							},
+				if (stat_points > 0) {
+
+					let char_stat = await CharacterStats.findOne({
+						where: {
+							character,
+							stat,
+						},
+					});
+
+					if (char_stat) {
+						let next_level = Number(char_stat.value + 1);
+
+						if (next_level > 0 && next_level < 6) {
+							await CharacterStats.update({
+								value: Number(char_stat.value + 1),
+							}, {
+								where: {
+									character,
+									stat,
+								},
+							});
+
+							await CharacterPoints.update({
+								stat_points: Sequelize.literal(`stat_points-'1'`),
+							}, {
+								where: {
+									character
+								},
+							});
+
+							response = true;
+							responseStatus = i18n.t('updateStat.done');
+						} else {
+							responseStatus = i18n.t('updateStat.maxLevel');
+						}
+					} else {
+						await CharacterStats.create({
+							character,
+							stat,
+							value: 1,
 						});
 
 						await CharacterPoints.update({
 							stat_points: Sequelize.literal(`stat_points-'1'`),
 						}, {
-							where:{
+							where: {
 								character
 							},
 						});
@@ -56,33 +92,16 @@ class StatsController {
 						response = true;
 						responseStatus = i18n.t('updateStat.done');
 					}
-					else{
-						responseStatus = i18n.t('updateStat.maxLevel');
-					}
 				} else {
-					await CharacterStats.create({
-						character,
-						stat,
-						value: 1,
-					});
-
-					await CharacterPoints.update({
-						stat_points: Sequelize.literal(`stat_points-'1'`),
-					}, {
-						where:{
-							character
-						},
-					});
-
-					response = true;
-					responseStatus = i18n.t('updateStat.done');
+					responseStatus = i18n.t('updateStat.pointsNeeded');
 				}
-			} else {
-				responseStatus = i18n.t('updateStat.pointsNeeded');
-			}
 
-		} else {
-			responseStatus = i18n.t('updateStat.NoPoints');
+			} else {
+				responseStatus = i18n.t('updateStat.NoPoints');
+			}
+		}
+		else{
+			responseStatus = i18n.t('permissionError');
 		}
 
 		return {
